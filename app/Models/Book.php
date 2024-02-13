a193dbea610d164d5b00604bcd8c0f1f984e06a7
frendikawa <frsnaifla@gmail.com>
2024-02-13 09:26:50 +0700

    Initial commit

---
+++ app/Models/Book.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Book extends Model
+{
+    use HasFactory;
+
+
+    /**
+     * Get all of the loans for the Book
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function loans(): HasMany
+    {
+        return $this->hasMany(Loan::class);
+    }
+}
---
+++ app/Models/Loan.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Loan extends Model
+{
+    use HasFactory;
+
+    /**
+     * Get the user that owns the Loan
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the book that owns the Loan
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function book(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}

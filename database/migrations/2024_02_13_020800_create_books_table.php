0825dd6404ba27634976d8ae27c853eb9e25f7aa
frendikawa <frsnaifla@gmail.com>
2024-02-13 09:28:41 +0700

    revisi 1

--- database/migrations/2024_02_13_020800_create_books_table.php
+++ database/migrations/2024_02_13_020800_create_books_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,7 +15,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('publisher');
             $table->string('author');
-            $table->boolean('availability')->default('true');
+            $table->integer('stock')->default(1);
             $table->timestamps();
         });
     }
